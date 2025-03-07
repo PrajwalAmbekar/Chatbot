2be7048fe5b7584e3242a03a58b06293deb8bfd0
Prajwal <ambekarprajwal33@gmail.com>
2025-03-07 23:07:55 +0530

    set up the sign, login,logout pages succesfully

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,21 +1,89 @@
 import User from "../models/user.model.js";
-export const signUp =async (req,res)=>{
-    const {fullName,email,passord}=req.body;
+import bcrypt from "bcryptjs";
+import generateToken from "../lib/utils.js";
 
-    if(passord.length<6){
-        res.status(400).json("Passord length must be greater than 6");
+
+
+
+export const signUp = async (req, res) => {
+    try {
+        const { fullName, email, password, profilePic } = req.body;
+        if (!fullName || !password || !email) {
+            res.status(400).json("Every field are required");
+        }
+
+        if (password.length < 6) {
+            res.status(400).json("Passord length must be greater than 6");
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            res.status(400).json("user already exits");
+        }
+        const salt = bcrypt.genSalt(10);
+        const hashedPassword = bcrypt.hash(password, salt);
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+
+            })
+        } else {
+            res.status(400).json("Error in creating newUser account")
+        }
+    }
+    catch(error){
+        console.log("Error in creating sign-up page",error.message);
+        res.status(500).json("Internal Server Error");
     }
 
-    const user=await User.findOne({email});
-    if(user){
-        res.status(400).json("user already exits")
+
+
+
+}
+
+export const login =async (req, res) => {
+   try {
+    const [email,password]=req.body;
+    const user=await new User.findOne({email});
+    if (!email){
+        res.status(400).json("Invalid Credentials");
     }
+    const isPasswordCorrect=bcrypt.compare(password,user.password);
+    if(!isPasswordCorrect){
+        res.status(400).json("Invalid credentials");
+    }
+
+    generateToken(newUser._id,res);
+    res.status(200).json({
+        _id:newUser._id,
+        fullName:newUser.fullName,
+        email:newUser.email,
+        profilePic:newUser.profilePic,
+    })
+    
+   } catch (error) {
+    console.log("Error in Login",error.message);
+    res.status(500).json("Onternal Server Error");
+   }
 }
 
-export const login =(req,res)=>{
-    res.send("login");
-}
-
-export const logout =(req,res)=>{
-    res.send("logout");
+export const logout = (req, res) => {
+    try {
+        res.cookies("jwt","",{masAge:0});
+        res.status(200).json("Logged out successfully");
+        
+    } catch (error) {
+        console.llog("Error in logout controller");
+        res.status(500).json("Internal Server Error");
+    }
 }
